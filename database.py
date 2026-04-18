6b391cc2283735ad3f30e20f843b2f9323f0241e
fabricehope1 <turikumwenayofabrice1@gmail.com>
2026-04-18 09:32:00 +0200

    Update database.py

--- database.py
+++ database.py
@@ -5,28 +5,12 @@ cursor=conn.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-telegram_id INTEGER,
-email TEXT UNIQUE
+telegram_id INTEGER PRIMARY KEY,
+email TEXT,
+demo_balance REAL DEFAULT 50000,
+real_balance REAL DEFAULT 0,
+energy INTEGER DEFAULT 100
 )
 """)
 
 conn.commit()
-
-
-def add_user(tg_id,email):
-    try:
-        cursor.execute(
-        "INSERT INTO users (telegram_id,email) VALUES (?,?)",
-        (tg_id,email))
-        conn.commit()
-        return True
-    except:
-        return False
-
-
-def get_user(tg_id):
-    cursor.execute(
-    "SELECT * FROM users WHERE telegram_id=?",
-    (tg_id,))
-    return cursor.fetchone()
